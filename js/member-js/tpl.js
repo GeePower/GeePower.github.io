bbf77aa1963510b1ffaff5232b5677a497406d92
GeePower <nvligpower@gmail.com>
2016-03-04 00:20:55 -0500

    add member

---
+++ js/member-js/tpl.js
@@ -0,0 +1,8 @@
+var tpl = "<div class=\"photo photo-center\">" +
+		"<div class=\"photo-wrap\">" +
+			"<div class=\"side-front side-common\">" +
+				"<img src=\"{{src}}\" alt=\"{{desc}}\" width=\"130\" height=\"180\">" +
+			"</div>" +
+			"<div class=\"side-back side-common\">{{desc}}</div>" +
+		"</div>" +
+	"</div>";
